0241fe779eded647a16b9dce98d0fcc0d8d8f971
nungo <nued65@gmail.com>
2025-10-13 13:38:34 +0300

    feat:added routing using react router

--- forum-test/frontend/src/App.jsx
+++ forum-test/frontend/src/App.jsx
@@ -1,9 +1,12 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
-
+import Signup from './pages/Signup'
+import Home from './pages/Home'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -13,7 +16,12 @@ function App() {
      {/* <div className="text-center mt-20 text-2xl text-blue-500">
       Hello Tailwind!
     </div> */}
-    <Login/>
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/signup" element={<Signup/>} />
+
+    </Routes>
       
     </>
   )
