47ba3c0fd83c76bfc1fe85767da716c8a8d798c4
nungo <nued65@gmail.com>
2025-10-15 15:55:46 +0300

    add:show the createpost form optionally

--- forum-test/frontend/src/components/Createpost.jsx
+++ forum-test/frontend/src/components/Createpost.jsx
@@ -1,5 +1,5 @@
 import{useState} from 'react'
-export default ()=>{
+export default (prop)=>{
      const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
 
@@ -13,7 +13,7 @@ export default ()=>{
     return (
         <>
         
-<div className="flex flex-col items-center shadow-lg p-20">
+<div className="flex flex-col items-center shadow-lg p-20  bg-black bg-opacity-50">
 
 <textarea  className="bg-gray-100 rounded w-100 focus:outline-none" rows="10" />
 <label htmlFor="upload-image" className='bg-gray-700 text-blue-500 p-1.5 rounded m-1'>Upload Image:</label>
@@ -28,7 +28,7 @@ export default ()=>{
           />
         </div>
       )}
-<button className="bg-gray-700 rounded p-1.5 text-blue-400 mt-1.5">Create Post</button>
+<button className="bg-gray-700 rounded p-1.5 text-blue-400 mt-1.5" onClick={prop.closeModal} >Create Post</button>
 
 
 
--- forum-test/frontend/src/pages/Home.jsx
+++ forum-test/frontend/src/pages/Home.jsx
@@ -2,13 +2,18 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar"
 import Post from "../components/Post"
 import CreatePost from "../components/Createpost"
+import {useState} from 'react'
 
 export default function Home(){
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
     return(
         <>
         <Navbar></Navbar>
         <div className="create-post flex flex-col items-center">
-        <CreatePost/>
+        {showModal&&<CreatePost closeModal={closeModal}/>}
+        {!showModal&&<button className="bg-gray-700 p-2 m-1 rounded text-blue-500" onClick={openModal}>Post</button>}
         </div>
         <div className=" grid  grid-cols-8 gap-5 items-stretch">
             <div className=" col-start-1 col-end-3 h-screen rounded-2xl p-5 shadow-2xl">{<Sidebar/>}</div>
