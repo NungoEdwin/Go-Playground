1581e8f17fccd32d2e9c9db231aabbeb9969f3e5
nungo <nued65@gmail.com>
2025-10-16 16:07:42 +0300

    chore:make post from the form possible

--- forum-test/frontend/src/components/Createpost.jsx
+++ forum-test/frontend/src/components/Createpost.jsx
@@ -3,6 +3,14 @@ import Modal from 'react-modal'
 export default (prop)=>{
      const [image, setImage] = useState(null);
   const [preview, setPreview] = useState(null);
+   const [textarea, setTextArea] = useState("");
+   const [title, setTitle] = useState("");
+
+const post={
+  id:Date.now(),
+  content:textarea,
+  title
+}
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -17,8 +25,8 @@ export default (prop)=>{
 overlayClassName="fixed inset-0 bg-transparent backdrop-blur-2xl flex items-center justify-center z-50"   
  className="flex flex-col items-center shadow-lg p-20  bg-white"
  contentLabel="CreatePost" isOpen={prop.showModal} onRequestClose={prop.closeModal}>
-
-<textarea  className="bg-gray-100 rounded w-100 focus:outline-none" rows="10" />
+<input  type="text" value={title} onChange={e=>setTitle(e.target.value)}/>
+<textarea  className="bg-gray-100 rounded w-100 focus:outline-none" rows="10" id  value={textarea} onChange={e=>setTextArea(e.target.value)}/>
 <label htmlFor="upload-image" className='bg-gray-700 text-blue-500 p-1.5 rounded m-1'>Upload Image:</label>
 <input type="file" accept="image/*" id="upload-image" onChange={handleImageChange}/>
   {preview && (
@@ -31,7 +39,7 @@ overlayClassName="fixed inset-0 bg-transparent backdrop-blur-2xl flex items-cent
           />
         </div>
       )}
-<button className="bg-gray-700 rounded p-1.5 text-blue-400 mt-1.5" onClick={prop.closeModal} >Create Post</button>
+<button className="bg-gray-700 rounded p-1.5 text-blue-400 mt-1.5" onClick={()=>{prop.CreatePost([...prop.Posts,post]); prop.closeModal()}} >Create Post</button>
 
 
 
@@ -41,3 +49,4 @@ overlayClassName="fixed inset-0 bg-transparent backdrop-blur-2xl flex items-cent
         </>
     )
 }
+ 
--- forum-test/frontend/src/pages/Home.jsx
+++ forum-test/frontend/src/pages/Home.jsx
@@ -18,12 +18,12 @@ export default function Home(){
         <>
         <Navbar></Navbar>
         <div className="create-post flex flex-col items-center">
-        {showModal&&<CreatePost closeModal={closeModal} showModal={showModal} CreatePost={setPosts}/>}
+        {showModal&&<CreatePost closeModal={closeModal} showModal={showModal} CreatePost={setPosts} Posts={posts}/>}
         {!showModal&&<button className="bg-gray-700 p-2 m-1 rounded text-blue-500" onClick={openModal}>Post</button>}
         </div>
         <div className=" grid  grid-cols-8 gap-5 items-stretch">
             <div className=" col-start-1 col-end-3 h-screen rounded-2xl p-5 shadow-2xl">{<Sidebar/>}</div>
-            <div className=" col-start-3 col-end-7  rounded p-5">{posts.map(post=><Post post={post}/>)}</div>
+            <div className=" col-start-3 col-end-7  rounded p-5">{posts.map(post=><Post key={post.id} post={post}/>)}</div>
             <div className=" col-start-7 col-end-9 shadow-xl rounded p-5">Chat Area</div>
         
         </div>
